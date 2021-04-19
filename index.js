7b30a2a799998e938383b458191b348055a36fac
kazden.jolley <kazden.jolley@gmail.com>
2021-04-19 12:45:10 -0600

    completed tasks 2-4

--- index.js
+++ index.js
@@ -62,8 +62,8 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+    return Math.floor(Math.random() * 3);
 }
 
 
@@ -81,8 +81,14 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inningCB, num){
+  let homeScore = 0;
+  let awayScore = 0;
+  for(let i = 0; i < num; i++){
+    homeScore += inningCB();
+    awayScore += inningCB();
+  }
+  return {Home: homeScore, Away: awayScore};
 }
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
@@ -90,8 +96,8 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningCB) {
+  return {Home: inningCB(), Away: inningCB()};
 }
 
 
